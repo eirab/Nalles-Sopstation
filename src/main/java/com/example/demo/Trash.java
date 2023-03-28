eecca5272cf69bba13e31297e904044d442e3190
Eira Birkhammar <eirabirkhammar@gmail.com>
2023-03-28 09:39:30 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/Trash.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class Trash {
+
+    private int trash_id;
+    private String name;
+    private TrashCategory category;
+
+
+
+}
---
+++ src/main/java/com/example/demo/TrashCategory.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public enum TrashCategory {
+    CARTON, PAPER, GLASS_COLORED, GLASS_CLEAR,
+    FOOD_WASTE;
+
+
+
+
+
+}
