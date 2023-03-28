fc875eb6fd6a8fabcba6d110c31a47bc80f5bd8f
FatiNajafi <123386850+FatiNajafi@users.noreply.github.com>
2023-03-28 15:13:58 +0200

    DatabSSTRUL

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,8 +4,8 @@ INSERT INTO TRASH-CATEGORY (id, name) VALUES (103, 'Glas');
 INSERT INTO TRASH-CATEGORY (id, name) VALUES (104, 'Batterier');
 INSERT INTO TRASH-CATEGORY (id, name) VALUES (105, 'Pappersförpackningar');
 
-INSERT INTO TRASH (id, name, trash-category) VALUES (201, 'Bananskal');
-INSERT INTO TRASH (id, name, trash-category) VALUES (201, 'Bananskal');
-INSERT INTO TRASH (id, name, trash-category) VALUES (201, 'Bananskal');
-INSERT INTO TRASH (id, name, trash-category) VALUES (201, 'Bananskal');
-INSERT INTO TRASH (id, name, trash-category) VALUES (201, 'Bananskal');
+INSERT INTO TRASH (id, name, trash-category) VALUES (201, 'Bananskal', 101);
+INSERT INTO TRASH (id, name, trash-category) VALUES (201, 'Plastic', 102);
+INSERT INTO TRASH (id, name, trash-category) VALUES (201, 'Glas', 103);
+INSERT INTO TRASH (id, name, trash-category) VALUES (201, 'Batteri', 104);
+INSERT INTO TRASH (id, name, trash-category) VALUES (201, 'Papperskartong',105);
