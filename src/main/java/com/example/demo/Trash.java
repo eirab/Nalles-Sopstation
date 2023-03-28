9c9622dc53685b394afaff4b47528086c431eb20
Eira Birkhammar <eirabirkhammar@gmail.com>
2023-03-28 09:45:35 +0200

    Updated trash class

--- src/main/java/com/example/demo/Trash.java
+++ src/main/java/com/example/demo/Trash.java
@@ -6,6 +6,33 @@ public class Trash {
     private String name;
     private TrashCategory category;
 
+    public Trash(int trash_id, String name, TrashCategory category) {
+        this.trash_id = trash_id;
+        this.name = name;
+        this.category = category;
+    }
 
+    public int getTrash_id() {
+        return trash_id;
+    }
 
+    public void setTrash_id(int trash_id) {
+        this.trash_id = trash_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public TrashCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(TrashCategory category) {
+        this.category = category;
+    }
 }
