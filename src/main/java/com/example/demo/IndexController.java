562caee30f730005589949cb7bd3737e1c84a21b
Eira <eirabirkhammar@gmail.com>
2023-03-30 17:30:15 +0200

    Poängräkningsfunktion implementerad

--- src/main/java/com/example/demo/IndexController.java
+++ src/main/java/com/example/demo/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,22 +17,21 @@ private TrashRepository trashRepository;
 @Autowired
 private TrashCanRepository trashCanRepository;
 
+
+
     @GetMapping("/")
     public String start(Model model ) {
-
-
 
         return "index";
     }
 
     @GetMapping("/spel")
     public String getGameplaySite(Model model) {
+
         model.addAttribute("trashArray", trashRepository.getTrash());
         model.addAttribute("trashCanArray",trashCanRepository.getTrashCans());
         return "game";
     }
-
-
 
 
     @GetMapping("/vinst")
