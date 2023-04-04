662a5615fc3f9b7a14a8053febf1af210ec50657
Zirha Welday <zirha-98@hotmail.com>
2023-04-04 11:38:31 +0200

    update ljud, ej klart

--- src/main/resources/static/sound.js
+++ src/main/resources/static/sound.js
@@ -1,2 +1,24 @@
- var audio = document.getElementById("startSong");
-  audio.play();
+
+  const startSong = document.getElementById('startSong');
+  const muteButton = document.getElementById('mute-button');
+  const volumeSlider = document.getElementById('volume-slider');
+
+   var audio = document.getElementById("startSong");
+    audio.play();
+
+// mute button
+muteButton.addEventListener("click", () => {
+  if (audio.muted) {
+    audio.muted = false;
+    muteButton.textContent = "Mute";
+  } else {
+    audio.muted = true;
+    muteButton.textContent = "Unmute";
+  }
+});
+
+// volume control
+volumeSlider.addEventListener("input", () => {
+  const volume = volumeSlider.value / 100;
+  audio.volume = volume;
+});
